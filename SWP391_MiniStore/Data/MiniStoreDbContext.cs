19fa044b4e148d9b55471fd4bf5c69ea834ed414
Zektroz <xzektrozx@gmail.com>
2023-06-05 19:59:42 +0700

    Staff Management Function

--- SWP391_MiniStore/Data/MiniStoreDbContext.cs
+++ SWP391_MiniStore/Data/MiniStoreDbContext.cs
@@ -78,55 +78,39 @@ namespace SWP391_MiniStore.Data
                 .Property(p => p.NetPay)
                 .HasPrecision(10, 2);
 
+            // Insert data
+            // Insert rows for Sales Shift
+            modelBuilder.Entity<WorkShift>().HasData(
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Sales", Holiday = false, DayOfWeek = null, StartTime = TimeSpan.Parse("06:00"), EndTime = TimeSpan.Parse("12:00"), WorkHours = 6, SalaryCoef = 1 },
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Sales", Holiday = false, DayOfWeek = null, StartTime = TimeSpan.Parse("12:00"), EndTime = TimeSpan.Parse("18:00"), WorkHours = 6, SalaryCoef = 1 },
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Sales", Holiday = false, DayOfWeek = null, StartTime = TimeSpan.Parse("18:00"), EndTime = TimeSpan.Parse("06:00"), WorkHours = 12, SalaryCoef = (decimal)1.5 },
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Sales", Holiday = false, DayOfWeek = "Sunday", StartTime = TimeSpan.Parse("18:00"), EndTime = TimeSpan.Parse("06:00"), WorkHours = 12, SalaryCoef = 2 },
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Sales", Holiday = true, DayOfWeek = null, StartTime = TimeSpan.Parse("18:00"), EndTime = TimeSpan.Parse("06:00"), WorkHours = 12, SalaryCoef = 3 }
+            );
+
+            // Insert rows for Guard Shift
+            modelBuilder.Entity<WorkShift>().HasData(
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Guard", Holiday = false, DayOfWeek = null, StartTime = TimeSpan.Parse("06:00"), EndTime = TimeSpan.Parse("18:00"), WorkHours = 12, SalaryCoef = 1 },
+                new WorkShift { ShiftID = Guid.NewGuid(), StaffRole = "Guard", Holiday = false, DayOfWeek = null, StartTime = TimeSpan.Parse("18:00"), EndTime = TimeSpan.Parse("06:00"), WorkHours = 12, SalaryCoef = (decimal)1.5 }
+            );
+
+            // Insert rows for Staff
+            modelBuilder.Entity<StoreStaff>().HasData(
+                // Manager
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "manager1", Email = "manager1@gmail.com", PhoneNumber = "1234567890", Password = "Password1#", Dob = new DateTime(1990, 1, 13), Address = "Address 1", StaffRole = "Manager", HourlyRate = new Random().Next(50, 71), StaffStatus = true },
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "manager2", Email = "manager2@gmail.com", PhoneNumber = "9876543210", Password = "Password2#", Dob = new DateTime(1990, 2, 14), Address = "Address 2", StaffRole = "Manager", HourlyRate = new Random().Next(50, 71), StaffStatus = true },
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "manager3", Email = "manager3@gmail.com", PhoneNumber = "1111111111", Password = "Password3#", Dob = new DateTime(1990, 3, 15), Address = "Address 3", StaffRole = "Manager", HourlyRate = new Random().Next(50, 71), StaffStatus = true },
+
+                // Sales
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "sales1", Email = "sales1@gmail.com", PhoneNumber = "2222222222", Password = "Password4#", Dob = new DateTime(1990, 4, 16), Address = "Address 4", StaffRole = "Sales", HourlyRate = new Random().Next(30, 51), StaffStatus = true },
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "sales2", Email = "sales2@gmail.com", PhoneNumber = "3333333333", Password = "Password5#", Dob = new DateTime(1990, 5, 17), Address = "Address 5", StaffRole = "Sales", HourlyRate = new Random().Next(30, 51), StaffStatus = true },
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "sales3", Email = "sales3@gmail.com", PhoneNumber = "4444444444", Password = "Password6#", Dob = new DateTime(1990, 6, 18), Address = "Address 6", StaffRole = "Sales", HourlyRate = new Random().Next(30, 51), StaffStatus = true },
+
+                // Guard
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "guard1", Email = "guard1@gmail.com", PhoneNumber = "5555555555", Password = "Password7#", Dob = new DateTime(1990, 7, 19), Address = "Address 7", StaffRole = "Guard", HourlyRate = new Random().Next(15, 31), StaffStatus = true },
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "guard2", Email = "guard2@gmail.com", PhoneNumber = "6666666666", Password = "Password8#", Dob = new DateTime(1990, 8, 20), Address = "Address 8", StaffRole = "Guard", HourlyRate = new Random().Next(15, 31), StaffStatus = true },
+                new StoreStaff { StaffID = Guid.NewGuid(), Username = "guard3", Email = "guard3@gmail.com", PhoneNumber = "7777777777", Password = "Password9#", Dob = new DateTime(1990, 9, 21), Address = "Address 9", StaffRole = "Guard", HourlyRate = new Random().Next(15, 31), StaffStatus = true }
+            );
         }
     }
 }
-
-
-
-//// Use this in migration
-
-//// Insert rows for Sales Shift
-//migrationBuilder.InsertData(
-//    table: "tblWorkShifts",
-//    columns: new[] { "ShiftID", "StaffRole", "Holiday", "DayOfWeek", "StartTime", "EndTime", "WorkHours", "SalaryCoef" },
-//    values: new object[,]
-//    {
-//                    { Guid.NewGuid(), "Sales", false, null, TimeSpan.Parse("06:00"), TimeSpan.Parse("12:00"), 6, 1 },
-//                    { Guid.NewGuid(), "Sales", false, null, TimeSpan.Parse("12:00"), TimeSpan.Parse("18:00"), 6, 1 },
-//                    { Guid.NewGuid(), "Sales", false, null, TimeSpan.Parse("18:00"), TimeSpan.Parse("06:00"), 12, 1.5 },
-//                    { Guid.NewGuid(), "Sales", false, "Sunday", TimeSpan.Parse("18:00"), TimeSpan.Parse("06:00"), 12, 2 },
-//                    { Guid.NewGuid(), "Sales", true, null, TimeSpan.Parse("18:00"), TimeSpan.Parse("06:00"), 12, 3 }
-//    });
-
-//// Insert rows for Guard Shift
-//migrationBuilder.InsertData(
-//    table: "tblWorkShifts",
-//    columns: new[] { "ShiftID", "StaffRole", "Holiday", "DayOfWeek", "StartTime", "EndTime", "WorkHours", "SalaryCoef" },
-//    values: new object[,]
-//    {
-//                    { Guid.NewGuid(), "Guard", false, null, TimeSpan.Parse("06:00"), TimeSpan.Parse("18:00"), 12, 1 },
-//                    { Guid.NewGuid(), "Guard", false, null, TimeSpan.Parse("18:00"), TimeSpan.Parse("06:00"), 12, 1.5 }
-//    });
-
-//// Insert rows for Staff
-//migrationBuilder.InsertData(
-//    table: "tblStoreStaff",
-//    columns: new[] { "StaffID", "Username", "Email", "PhoneNumber", "Password", "Dob", "Address", "StaffRole", "HourlyRate", "StaffStatus" },
-//    values: new object[,]
-//    {
-//                    // Manager
-//                    { Guid.NewGuid(), "manager1", "manager1@gmail.com", "1234567890", "Password1#", new DateTime(1990, 1, 13), "Address 1", "Manager", new Random().Next(50, 71), true },
-//                    { Guid.NewGuid(), "manager2", "manager2@gmail.com", "9876543210", "Password2#", new DateTime(1990, 2, 14), "Address 2", "Manager", new Random().Next(50, 71), true },
-//                    { Guid.NewGuid(), "manager3", "manager3@gmail.com", "1111111111", "Password3#", new DateTime(1990, 3, 15), "Address 3", "Manager", new Random().Next(50, 71), true },
-
-//                    // Sales
-//                    { Guid.NewGuid(), "sales1", "sales1@gmail.com", "2222222222", "Password4#", new DateTime(1990, 4, 16), "Address 4", "Sales", new Random().Next(30, 51), true },
-//                    { Guid.NewGuid(), "sales2", "sales2@gmail.com", "3333333333", "Password5#", new DateTime(1990, 5, 17), "Address 5", "Sales", new Random().Next(30, 51), true },
-//                    { Guid.NewGuid(), "sales3", "sales3@gmail.com", "4444444444", "Password6#", new DateTime(1990, 6, 18), "Address 6", "Sales", new Random().Next(30, 51), true },
-
-//                    // Guard
-//                    { Guid.NewGuid(), "guard1", "guard1@gmail.com", "5555555555", "Password7#", new DateTime(1990, 7, 19), "Address 7", "Guard", new Random().Next(15, 31), true },
-//                    { Guid.NewGuid(), "guard2", "guard2@gmail.com", "6666666666", "Password8#", new DateTime(1990, 8, 20), "Address 8", "Guard", new Random().Next(15, 31), true },
-//                    { Guid.NewGuid(), "guard3", "guard3@gmail.com", "7777777777", "Password9#", new DateTime(1990, 9, 21), "Address 9", "Guard", new Random().Next(15, 31), true }
-//    });
